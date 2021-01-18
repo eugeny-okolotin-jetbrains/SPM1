e9f4e11f198894befae7c19b1915620cb1836836
Eugeny Okolotin <eugeny.okolotin@jetbrains.com>
2021-01-18 17:41:23 +0300

    zzz

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        //.target(
             name: "SPM1",
             dependencies: []
             //resources: [.process("QQQ.png")]
